f04b1963c0973b7517f23b0a189f810ea1f3b94a
EkaterinaTR <ekaterinatsr@gmail.com>
2021-04-01 14:03:56 +0300

    new link

--- spider.py
+++ spider.py
@@ -101,8 +101,8 @@ def create_list(graf):
 
 
 # main
-url = 'https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0'
-deep = 4
+url = 'https://www.google.com/?gws_rd=ssl'
+deep = 3
 url_table = {}
 id_table = {}
 graf = {}
